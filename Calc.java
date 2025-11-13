b6585f454debca2ae0c14e04ad3604da15e6d5e1
hamhamad82 <hamhamad82@gmail.com>
2025-11-13 16:58:51 +0200

    adding comments

--- Calc.java
+++ Calc.java
@@ -8,20 +8,22 @@ public class Calc {
     public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
         System.out.print("Enter the operation(+,-,*,/); ");
-      String operation = input.nextLine().trim();
-      String[] parts = new String[2];
+      String operation = input.nextLine().trim(); 
+      String[] parts = new String[2]; // contains the 2 numbers to be calculated
       char operator = ' ';
       if(operation.contains("+")){ parts = operation.split("\\+");operator = '+'; }
       else if(operation.contains("-")){parts = operation.split("-");operator = '-';}
       else if(operation.contains("*")){parts = operation.split("\\*");operator = '*';}
       else if(operation.contains("/")){parts = operation.split("/");operator = '/';}
-      else {System.out.println("invalid input");}
+      else {System.out.println("invalid input");} // fill parts array, assign operator var
       
+      //parsing the numbers from parts to calculate them
       double result = 0;
-      double num1 = Double.parseDouble(parts[0].trim());
+      double num1 = Double.parseDouble(parts[0].trim()); 
       double num2 = Double.parseDouble(parts[1].trim());
       
-      switch (operator) {
+      //calculation
+      switch (operator) { 
             case '+': result = num1 + num2; break;
             case '-': result = num1 - num2; break;
             case '*': result = num1 * num2; break;
